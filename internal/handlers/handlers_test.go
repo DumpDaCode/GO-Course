1461210a28e81fc1a88d0585fa9d2356138524f9
Rajivranjan Singh <rajivranjans@mkcl.org>
2022-08-23 10:09:28 +0530

    Done with post request testing.

--- internal/handlers/handlers_test.go
+++ internal/handlers/handlers_test.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"testing"
 )
 
@@ -18,54 +19,27 @@ var theTests = []struct {
 	params             []postData
 	expectedStatusCode int
 }{
-	{
-		"Home",
-		"/",
-		"GET",
-		[]postData{},
-		http.StatusOK,
-	},
-	{
-		"about",
-		"/about",
-		"GET",
-		[]postData{},
-		http.StatusOK,
-	},
-	{
-		"Generals Quarters",
-		"/generals-quarters",
-		"GET",
-		[]postData{},
-		http.StatusOK,
-	},
-	{
-		"Major's Suite",
-		"/majors-suite",
-		"GET",
-		[]postData{},
-		http.StatusOK,
-	}, {
-		"Search Availability",
-		"/search-availability",
-		"GET",
-		[]postData{},
-		http.StatusOK,
-	},
-	{
-		"Contact",
-		"/contact",
-		"GET",
-		[]postData{},
-		http.StatusOK,
-	},
-	{
-		"Make Reservation",
-		"/make-reservation",
-		"GET",
-		[]postData{},
-		http.StatusOK,
-	},
+	{"Home", "/", "GET", []postData{}, http.StatusOK},
+	{"about", "/about", "GET", []postData{}, http.StatusOK},
+	{"Generals Quarters", "/generals-quarters", "GET", []postData{}, http.StatusOK},
+	{"Major's Suite", "/majors-suite", "GET", []postData{}, http.StatusOK},
+	{"Search Availability", "/search-availability", "GET", []postData{}, http.StatusOK},
+	{"Contact", "/contact", "GET", []postData{}, http.StatusOK},
+	{"Make Reservation", "/make-reservation", "GET", []postData{}, http.StatusOK},
+	{"Post Search Availability", "/search-availability", "POST", []postData{
+		{key: "start", value: "2020-01-01"},
+		{key: "end", value: "2020-01-01"},
+	}, http.StatusOK},
+	{"Post Search Availability JSON", "/search-availability-json", "POST", []postData{
+		{key: "start", value: "2020-01-01"},
+		{key: "end", value: "2020-01-01"},
+	}, http.StatusOK},
+	{"Post make reservation", "/make-reservation", "POST", []postData{
+		{key: "first_name", value: "Rajiv"},
+		{key: "last_name", value: "singh"},
+		{key: "email", value: "me@here.com"},
+		{key: "phone", value: "555-555-5555"},
+	}, http.StatusOK},
 }
 
 func TestHandlers(t *testing.T) {
@@ -76,6 +50,19 @@ func TestHandlers(t *testing.T) {
 	for _, e := range theTests {
 		if e.method == "GET" {
 			resp, err := ts.Client().Get(ts.URL + e.url)
+			if err != nil {
+				t.Log(e)
+				t.Fatal(err)
+			}
+			if resp.StatusCode != e.expectedStatusCode {
+				t.Errorf("for %s expected %d but got %d", e.name, e.expectedStatusCode, resp.StatusCode)
+			}
+		} else {
+			values := url.Values{}
+			for _, x := range e.params {
+				values.Add(x.key, x.value)
+			}
+			resp, err := ts.Client().PostForm(ts.URL+e.url, values)
 			if err != nil {
 				t.Log(e)
 				t.Fatal(err)
