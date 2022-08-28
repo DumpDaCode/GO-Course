d0c5b4db84f20f3c8487e95b8dbbcc2bf6d165ca
RajivranjanS <rajivranjans@mkcl.org>
2022-08-28 11:54:06 +0100

    Added logout functionality

--- internal/models/templateData.go
+++ internal/models/templateData.go
@@ -4,13 +4,14 @@ import "github.com/go-course/bookings/internal/forms"
 
 // TemplateData holds data send from handlers to template
 type TemplateData struct {
-	StringMap map[string]string
-	IntMap    map[string]int
-	FloatMap  map[string]float64
-	Data      map[string]interface{}
-	CSRFToken string
-	Flash     string
-	Warning   string
-	Error     string
-	Form      *forms.Form
+	StringMap       map[string]string
+	IntMap          map[string]int
+	FloatMap        map[string]float64
+	Data            map[string]interface{}
+	CSRFToken       string
+	Flash           string
+	Warning         string
+	Error           string
+	Form            *forms.Form
+	IsAuthenticated int
 }
