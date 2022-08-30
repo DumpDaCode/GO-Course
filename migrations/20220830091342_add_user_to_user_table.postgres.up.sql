24b8be739de6fb56cddba3ea7c1f701aefef911c
Rajivranjan Singh <rajivranjans@mkcl.org>
2022-08-30 15:03:47 +0530

    Migrations for users

---
+++ migrations/20220830091342_add_user_to_user_table.postgres.up.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.users (first_name,last_name,email,"password",access_level,created_at,updated_at) VALUES
+	 ('rajiv','singh','rajiv@mkcl.org','$2a$12$yIjkO2fr5bLiC2cDrQXZ1uqfUOmVnxuVh4OgO6nDKDBDB5eQdyUPy',3,'2022-08-30 00:00:00.000','2022-08-30 00:00:00.000');
